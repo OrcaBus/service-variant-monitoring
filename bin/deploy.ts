851cc928cea5cbb68d3405a14a41be90c5e10d58
Quentin Clayssen <quentin.clayssen@gmail.com>
2026-04-23 14:17:24 +1000

    remove stateful-stack trace

--- bin/deploy.ts
+++ bin/deploy.ts
@@ -2,7 +2,6 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { StatelessStack } from '../infrastructure/toolchain/stateless-stack';
-import { StatefulStack } from '../infrastructure/toolchain/stateful-stack';
 import { TOOLCHAIN_ENVIRONMENT } from '@orcabus/platform-cdk-constructs/deployment-stack-pipeline';
 const app = new cdk.App();
 
@@ -15,10 +14,6 @@ if (deployMode === 'stateless') {
   new StatelessStack(app, 'OrcaBusStatelessVariantMonitoringStack', {
     env: TOOLCHAIN_ENVIRONMENT,
   });
-} else if (deployMode === 'stateful') {
-  new StatefulStack(app, 'OrcaBusStatefulVariantMonitoringStack', {
-    env: TOOLCHAIN_ENVIRONMENT,
-  });
 } else {
-  throw new Error("Invalid 'deployMode` set in the context");
+  throw new Error("Invalid 'deployMode' set in the context (expected: stateless)");
 }
